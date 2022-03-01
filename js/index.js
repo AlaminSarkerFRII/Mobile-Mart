62940774621e0e01b6006b4a6f78f4788f2288ed
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-03-01 12:47:12 +0600

    clear Main Div And Single Phone div

--- js/index.js
+++ js/index.js
@@ -1,76 +1,78 @@
 
 
-// declare container globally 
+// declare both container globally 
+let phoneDetails =document.getElementById('phone-Details');
 const containerDiv = document.getElementById('displayAll');
-
 const loadPhone = () => {
     const input = document.getElementById('input-field');
+    const errorMessage = document.getElementById('error-message'); // error sms id 
+    containerDiv.innerHTML = "";//clear main div
+    phoneDetails.innerHTML = "";//clear phoneDetails
     const inputValue = input.value;
     input.value = "";
-// get all mobile phone 
-     containerDiv.innerHTML = "";
-    const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`
-    fetch(url)
-    .then((res)=>res.json())
-    .then((data) => displayPhone(data.data))
+    // error message
+if(inputValue == ""){
+  errorMessage.innerText = "please give a Phone Name";
+  input.value = "" //field clear
+  containerDiv.innerHTML = "" // main div clear hobe
+
+} else{
+  // get all mobile phone 
+  const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`
+  fetch(url)
+  .then((res)=>res.json())
+  .then((data) => displayPhone(data.data.slice(0,20)))
+  input.value = "";
+  errorMessage.innerText = "" // error sms por empty hobe
+}
+
 }
 
 const displayPhone = (phones) =>{
     for (const phone of phones){
-        // console.log(phone);
+      console.log(phone);
         const div = document.createElement('div'); // create div and append
         div.classList.add('col-md-6')
         div.classList.add('col-sm-12')
-        div.innerHTML = `<div class="card m-4 text-center mb-3" style="width: 18rem;">
+        div.innerHTML = `<div class="card mx-4 text-center mb-3" style="width: 18rem;">
         <img class="w-75 mx-auto p-3" src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
             <h4>Brand :${phone.brand}</h4>
-          <h5 class="card-title">Name : ${phone.phone_name} </h5>
-          <a href="#" class="btn btn-primary" onClick="phoneDetails('${phone.slug}')">See Details</a>
+          <h5 >Name : ${phone.phone_name} </h5>
+          <h5 >Name : ${phone.memory} </h5>
+          <a href="#" class="btn btn-primary" onClick="getPhoneDetails('${phone.slug}')">See Details</a>
         </div>
       </div>`;
       containerDiv.appendChild(div);
-      
     }
 }
 
-const phoneDetails = id =>{
+//get phone details 
+
+const getPhoneDetails = id =>{
 fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
 .then((res) => res.json())
 .then((data) => displayPhoneDetails(data.data));
 
 }
-    // const allPhones = (data.data)
-  //   // console.log(allPhones);
-  //   const singlePhone = allPhones.find(phone =>phone.id===id )
-  //   const div = document.createElement('div');
-  //   containerDiv.innerHTML = "";
-  //   div.innerHTML = `<div class="card text-center mb-3" style="width: 18rem;">
-  //   <img class="w-75 mx-auto p-3" src="${singlePhone.image}" class="card-img-top" alt="...">
-  //   <div class="card-body">
-  //       <h4>Brand :${singlePhone.brand}</h4>
-  //     <h5 class="card-title">Name : ${singlePhone.memory} </h5>
-  //   </div>
-  // </div>`; 
 
-  // details.appendChild(div);
-
+// phone details display
   const displayPhoneDetails = (phone) =>{
     console.log(phone);
-    const phoneDetails =document.getElementById('phone-Details')
+    containerDiv.innerHTML = ""; // cleard main div
+    phoneDetails.innerHTML = "";
     const div = document.createElement('div');
-   div.classList.add('card')
-    div.innerHTML = `<div class="card text-center mb-3" style="width: 18rem;">
+    div.innerHTML = `<div class="card text-center mb-3 w-100 mx-auto">
     <img class="w-75 mx-auto p-3" src="${phone.image}" class="card-img-top" alt="...">
     <div class="card-body">
-        <h4>Brand :${phone.brand}</h4>
-      <h5 class="card-title">Storages : ${phone.mainFeatures.storage} </h5>
-      <p> ${phone.others}</p>
+        <h5>Name :${phone.name}</h5>
+      <h5>Storages : ${phone.mainFeatures.storage} </h5>
+      <h6> ${phone.slug}</h6>
+      <h6> ${phone.releaseDate}</h6>
+      <button class="btn btn-primary">Show More...</button>
     </div>
-  </div>`
-
+  </div>`;
   phoneDetails.appendChild(div)
 
   }
 
-  
